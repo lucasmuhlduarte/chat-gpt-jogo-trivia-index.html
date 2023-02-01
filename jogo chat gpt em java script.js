fea222bbff74a01af7d0d9d91c28722e519cb21e
lucasmuhlduarte <106273123+lucasmuhlduarte@users.noreply.github.com>
2023-01-31 23:01:06 -0300

    Create jogo chat gpt em java script.js

---
+++ jogo chat gpt em java script.js
@@ -0,0 +1,75 @@
+<!DOCTYPE html>
+<html>
+<head>
+<script>
+function checkAnswer1() {
+  var x = document.getElementById("answer1").value;
+  if (x === "a") {
+    alert("Correct answer!");
+  } else {
+    alert("Incorrect answer.");
+  }
+}
+
+function checkAnswer2() {
+  var x = document.getElementById("answer2").value;
+  if (x === "a") {
+    alert("Correct answer!");
+  } else {
+    alert("Incorrect answer.");
+  }
+}
+
+function checkAnswer3() {
+  var x = document.getElementById("answer3").value;
+  if (x === "a") {
+    alert("Correct answer!");
+  } else {
+    alert("Incorrect answer.");
+  }
+}
+
+function checkAnswer4() {
+  var x = document.getElementById("answer4").value;
+  if (x === "a") {
+    alert("Correct answer!");
+  } else {
+    alert("Incorrect answer.");
+  }
+}
+
+function checkAnswer5() {
+  var x = document.getElementById("answer5").value;
+  if (x === "a") {
+    alert("Correct answer!");
+  } else {
+    alert("Incorrect answer.");
+  }
+}
+</script>
+</head>
+<body>
+
+<p>Question 1: In what year did the French Revolution begin?</p>
+<button onclick="checkAnswer1()">a) 1789</button>
+<input type="hidden" id="answer1" value="a">
+<button onclick="checkAnswer1()">b) 1799</button>
+<input type="hidden" id="answer1" value="b">
+<button onclick="checkAnswer1()">c) 1804</button>
+<input type="hidden" id="answer1" value="c">
+
+<p>Question 2: Who painted the Mona Lisa?</p>
+<button onclick="checkAnswer2()">a) Leonardo da Vinci</button>
+<input type="hidden" id="answer2" value="a">
+<button onclick="checkAnswer2()">b) Michelangelo</button>
+<input type="hidden" id="answer2" value="b">
+<button onclick="checkAnswer2()">c) Rafael</button>
+<input type="hidden" id="answer2" value="c">
+
+<p>Question 3: In what year was the Declaration of Independence of the United States signed?</p>
+<button onclick="checkAnswer3()">a) 1776</button>
+<input type="hidden" id="answer3" value="a">
+<button onclick="checkAnswer3()">b) 1787</button>
+<input type="hidden" id="answer3" value="b">
+<button onclick="checkAnswer3()">c) 1791</button>
+<input type="hidden
